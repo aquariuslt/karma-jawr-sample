f385a9ef43067cc02892dc6a41cb19cc5da4303b
Aquariuslt <superaquariuslt@gmail.com>
2017-09-21 01:59:57 +0800

    [Setup] Add JSF Configs.

--- src/main/java/agile/web/HomeController.java
+++ src/main/java/agile/web/HomeController.java
@@ -1,7 +1,9 @@
 package agile.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /* Created by Jason Cui on 2017-09-20.*/
@@ -12,4 +14,5 @@ public class HomeController {
     public ModelAndView homeView() {
         return new ModelAndView("index");
     }
+
 }
