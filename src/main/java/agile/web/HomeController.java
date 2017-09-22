bfd8a1b27218af2ed0bb91525dad180d556730c0
Jason Cui <jason.cui@oocl.com>
2017-09-22 17:33:41 +0800

    [JAWR + Ext3] Add ext3 default css and correct Spring Config settings.

--- src/main/java/agile/web/HomeController.java
+++ src/main/java/agile/web/HomeController.java
@@ -1,5 +1,7 @@
 package agile.web;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    private Log log = LogFactory.getLog(HomeController.class);
+
+
     @RequestMapping("/")
-    public ModelAndView homeView() {
+    public ModelAndView indexView() {
+        log.info("render index page");
         return new ModelAndView("index");
     }
 
-    @GetMapping("/hello")
+    @RequestMapping("/home")
+    public ModelAndView homeView() {
+        log.info("render home page");
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping("/hello")
     @ResponseBody
     public String helloResponse() {
         return "hello";
